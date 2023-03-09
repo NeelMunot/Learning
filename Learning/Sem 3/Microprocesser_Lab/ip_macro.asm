2b0a7e1b2653c4306ecbc466ce1f93ec08ccea38
Neel Munot <91302244+NeelMunot@users.noreply.github.com>
2023-03-09 19:50:09 +0530

    after mpl

--- Learning/Sem 3/Microprocesser_Lab/ip_macro.asm
+++ Learning/Sem 3/Microprocesser_Lab/ip_macro.asm
@@ -7,19 +7,21 @@
 %endmacro
 
 section .data
-msg db "inside macro", 0ah,0dh
-len equ $-msg
 
+section .bss
+num1 resb 2
 
-
-section .bss 
-
-section .text
+section .text 
 global _start
 _start:
+mov rax,00h
+mov rdi,00h
+mov rsi, num1
+mov rdx, 02
+syscall
 
-disp msg, len
+disp num1, 02
 
 mov rax,3ch
-mov rdi, 00h
+mov rdi,00h
 syscall
